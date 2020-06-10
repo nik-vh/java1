3663df7142a2ca6c732a4164f320378cec59a326
nik-vh <nikowsun@gmail.com>
2020-06-10 20:43:10 +0300

    Lesson8_1

--- lesson8/src/ru/geekbrains/lesson8/Window.java
+++ lesson8/src/ru/geekbrains/lesson8/Window.java
@@ -8,34 +8,36 @@ public class Window extends JFrame {
     public  Window() {
         JFrame frame = new JFrame("Текстовый редактор");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 600);
+        frame.setBounds(500, 100, 400, 400);
+        frame.setSize(800, 800);
 
-        JMenuBar mb = new JMenuBar();
-        JMenu m1 = new JMenu("Файл");
-        JMenu m2 = new JMenu("Сравка");
-        mb.add(m1);
-        mb.add(m2);
-        JMenuItem m11 = new JMenuItem("Открыть");
-        JMenuItem m12 = new JMenuItem("Сохранить как");
-        JMenuItem m13 = new JMenuItem("Выход");
-        JMenuItem m21 = new JMenuItem("Помощь");
-        m1.add(m11);
-        m1.add(m12);
-        m1.add(m13);
-        m2.add(m21);
+        JMenuBar mainMenuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Файл");
+        JMenu refMenu = new JMenu("Сравка");
+        mainMenuBar.add(fileMenu);
+        mainMenuBar.add(refMenu);
+        JMenuItem fm1 = new JMenuItem("Открыть");
+        JMenuItem fm2 = new JMenuItem("Сохранить как");
+        JMenuItem fm3 = new JMenuItem("Выход");
+        JMenuItem rm1 = new JMenuItem("Помощь");
+        fileMenu.add(fm1);
+        fileMenu.add(fm2);
+        fileMenu.add(fm3);
+        refMenu.add(rm1);
 
-        m13.addActionListener(new ActionListener() {
+        fm3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
 
-        m21.addActionListener(new ActionListener() {
+        rm1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFrame helpFrame = new JFrame("Помощь");
                 //helpFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                helpFrame.setBounds(750, 350, 400, 400);
                 helpFrame.setSize(300, 300);
                 helpFrame.setVisible(true);
 
@@ -45,17 +47,17 @@ public class Window extends JFrame {
         JPanel panel = new JPanel();
         JLabel label = new JLabel("Поиск");
         JTextField tf = new JTextField(10);
-        JButton send = new JButton("Найти");
-        JButton reset = new JButton("Удалить");
+        JButton search = new JButton("Найти");
+        JButton replace = new JButton("Заменить");
         panel.add(label);
         panel.add(tf);
-        panel.add(send);
-        panel.add(reset);
+        panel.add(search);
+        panel.add(replace);
 
         JTextArea ta = new JTextArea();
 
         frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.NORTH, mainMenuBar);
         frame.getContentPane().add(BorderLayout.CENTER, ta);
         frame.setVisible(true);
     }
